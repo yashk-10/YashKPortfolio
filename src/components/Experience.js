f4363f0d07cc00572c054b572a1c6e99ef8f7730
YashKapoor1004 <yashkapoor1004@gmail.com>
2022-07-19 23:27:39 -0400

    Experience Section Complete

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Work from './Work'
 import Data from './Data'
 import ExperienceMain from '../assets/ExperienceMain.png'
@@ -6,9 +6,34 @@ import '../styles/experience.css'
 
 const Experience = () => {
   const [active, setActive] = useState("Qoom");
+  const Qoom = useRef()
+  const Felix = useRef()
+  const NodeApp = useRef()
 
   useEffect(() => {
-    
+    if(active === "Qoom"){
+      Qoom.current.style.fontWeight = "700";
+      Qoom.current.style.color = "#6FFFD4";
+      Felix.current.style.fontWeight = "200";
+      Felix.current.style.color = "white";
+      NodeApp.current.style.fontWeight = "200";
+      NodeApp.current.style.color = "white";
+    } else if (active === "Felix"){
+      Felix.current.style.fontWeight = "700";
+      Felix.current.style.color = "#6FFFD4";
+      Qoom.current.style.fontWeight = "200";
+      Qoom.current.style.color = "white";
+      NodeApp.current.style.fontWeight = "200";
+      NodeApp.current.style.color = "white";
+    } else if (active === "Node"){
+      NodeApp.current.style.fontWeight = "700";
+      NodeApp.current.style.color = "#6FFFD4";
+      Felix.current.style.fontWeight = "200";
+      Felix.current.style.color = "white";
+      Qoom.current.style.fontWeight = "200";
+      Qoom.current.style.color = "white";
+    }
+
   }, [active]);
 
 
@@ -22,9 +47,9 @@ const Experience = () => {
         <p className="exp-summary">Attaining Industry-Specific Skills at Growing Companies.</p>
         <img className="exp-img" src={ExperienceMain}></img>
         <div className="exp-companies">
-          <a onClick={() => setActive("Qoom")} id="Qoom" className="company">Qoom</a>
-          <a onClick={() => setActive("Felix")} id="Felix" className="company">Felix</a>
-          <a onClick={() => setActive("Node")} id="Node" className="company">Node App</a>
+          <a ref={Qoom} onClick={() => setActive("Qoom")} id="Qoom" className="company">Qoom</a>
+          <a ref={Felix} onClick={() => setActive("Felix")} id="Felix" className="company">Felix</a>
+          <a ref={NodeApp} onClick={() => setActive("Node")} id="Node" className="company">Node App</a>
         </div>
         <div className="exp-content">
           {active === "Qoom" && <Work data={Data} id={0}/>}
