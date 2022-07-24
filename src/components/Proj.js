7d33928d45fb393f81ac8c05617c438563f7d5a2
YashKapoor1004 <yashkapoor1004@gmail.com>
2022-07-24 16:00:25 -0400

    Projects Section 40% Complete

--- src/components/Proj.js
+++ src/components/Proj.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/experience.css'
+import '../styles/projects.css'
 
 const Proj = ({ data, pic, id }) => {
   return (
@@ -9,20 +9,20 @@ const Proj = ({ data, pic, id }) => {
               <div className="proj-left">
                 <img className="proj-img" src={pic}/>
                 <div className="proj-top">
-                  <h1 className="proj-tool"></h1>
-                  <h1 className="proj-tool"></h1>
-                  <h1 className="proj-tool"></h1>
+                  <h1 className="proj-tool">{item.tools[0]}</h1>
+                  <h1 className="proj-tool">{item.tools[1]}</h1>
+                  <h1 className="proj-tool">{item.tools[2]}</h1>
                 </div>
                 <div className="proj-bottom">
-                  <h1 className="proj-tool"></h1>
-                  <h1 className="proj-tool"></h1>
-                  <h1 className="proj-tool"></h1>
-                  <h1 className="proj-tool"></h1>
+                  <h1 className="proj-tool">{item.tools[3]}</h1>
+                  <h1 className="proj-tool">{item.tools[4]}</h1>
+                  <h1 className="proj-tool">{item.tools[5]}</h1>
+                  <h1 className="proj-tool">{item.tools[6]}</h1>
                 </div>
               </div>
               <div className="proj-right">
-                <h1 className="proj-name"></h1>
-                <p className="proj-desc"></p>
+                <h1 className="proj-name">{item.name}</h1>
+                <p className="proj-desc">{item.desc}</p>
               </div>
             </div>
         ))}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,13 +7,13 @@ import '../styles/projects.css'
 const Projects = () => {
   return (
     <div className="projects">
-        <div className="proj-top">
-            <div className="proj-line"></div>
-            <h1 className="proj-head">Projects</h1>
+        <div className="projects-top">
+            <div className="projects-line"></div>
+            <h1 className="projects-head">Projects</h1>
         </div>
-        <p className="proj-summary">Bringing Innovative Ideas to Life.</p>
-        {/* <Proj data={ProjData} pic={Lan} id={0}/>
-        <Proj data={ProjData} id={1}/>
+        <p className="projects-summary">Bringing Innovative Ideas to Life.</p>
+        <Proj data={ProjData} pic={Lan} id={0}/>
+        {/* <Proj data={ProjData} id={1}/>
         <Proj data={ProjData} id={2}/> */}
     </div>
   )
