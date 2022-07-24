485d3116a78b0f1b26a568ec18efbed095484963
YashKapoor1004 <yashkapoor1004@gmail.com>
2022-07-23 23:27:30 -0400

    Project Section 30% Complete

--- src/components/Proj.js
+++ src/components/Proj.js
@@ -6,7 +6,24 @@ const Proj = ({ data, pic, id }) => {
     <div>
         {data[id].map(item =>(
             <div className="proj">
-                <img src={pic}/>
+              <div className="proj-left">
+                <img className="proj-img" src={pic}/>
+                <div className="proj-top">
+                  <h1 className="proj-tool"></h1>
+                  <h1 className="proj-tool"></h1>
+                  <h1 className="proj-tool"></h1>
+                </div>
+                <div className="proj-bottom">
+                  <h1 className="proj-tool"></h1>
+                  <h1 className="proj-tool"></h1>
+                  <h1 className="proj-tool"></h1>
+                  <h1 className="proj-tool"></h1>
+                </div>
+              </div>
+              <div className="proj-right">
+                <h1 className="proj-name"></h1>
+                <p className="proj-desc"></p>
+              </div>
             </div>
         ))}
     </div>
